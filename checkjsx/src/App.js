5d384eb8f2320d19a9579df8d8ef177034a11f12
RaNda-Haddad <randahaddad8@gmail.com>
2023-02-16 07:24:36 +0100

    first commit

---
+++ checkjsx/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Carousels from './components/Crousels';
+import Forms from './components/Forms';
+import Navb from './components/Navb';
+import Titles from './components/Titles';
+
+function App() {
+  return (
+    <div className="App">
+
+<Navb/>
+
+  <Titles/>
+
+<Forms/>
+
+<Carousels/>
+
+
+  </div>
+  );
+}
+
+export default App;
---
+++ checkjsx/src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Titles = () => {
+  return (
+    <div>
+
+<h1> You Are Welcom </h1>
+
+    </div>
+  )
+}
+
+export default Titles
